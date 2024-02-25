6a61192a9b4581dbd0358760324b5e19c43cd199
imJudah <judahjmdasuki@gmail.com>
2024-02-25 20:02:48 +0700

    create a toolbar and initialize header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: ``,
+    template: `<app-header /> <router-outlet></router-outlet>`,
     styles: [],
 })
 export class AppComponent {
